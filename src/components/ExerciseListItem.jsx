567d03b91b72dd0115439ae424e71211adcaadb2
JackOceanGate <smashygamer@hotmail.com>
2024-07-11 18:55:11 +0100

    added stack & description page

--- src/components/ExerciseListItem.jsx
+++ src/components/ExerciseListItem.jsx
@@ -1,15 +1,17 @@
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, Pressable } from 'react-native';
+import {Link} from 'expo-router';
 
 export default function ExerciseListItem({ item }){
     return(
-    <View style={styles.exerciseContainer}>
-      <Text style={styles.exerciseName}>
-        {item.name}</Text>
+    <Link href={`/${item.name}`} asChild>
+    <Pressable style={styles.exerciseContainer}>
+      <Text style={styles.exerciseName}>{item.name}</Text>
       <Text style={styles.exerciseSubtitle}>
-        <Text style={styles.subValue}>{item.muscle}</Text> | <Text style={styles.subValue}>{item.equipment} </Text>
-        </Text>
-    </View>
+      <Text style={styles.subValue}>{item.muscle}</Text> | 
+      <Text style={styles.subValue}>{item.equipment} </Text>
+      </Text>
+    </Pressable>
+    </Link>
     )
   }
 
